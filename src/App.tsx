837d99a7dd8e977b9bd6506c4c553d9b82d9500e
Dmitry Verteyko <verteyko1990@gmail.com>
2021-06-22 23:02:23 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
